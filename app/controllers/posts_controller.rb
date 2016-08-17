91c89a641804d9123872aeabc62bbde0974130d6
maimitp9@gmail.com <maimitp9@gmail.com>
2016-08-17 09:28:59 +0530

    add ajax-crud first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,70 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  # GET /posts/1
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.js 
+      else
+        format.html { render :new }
+        format.js
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.js
+      else
+        format.html { render :edit }
+        format.js
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.js
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :cotent)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+	#validates :title, :presence => {:message => "Please enter title" }
+	#validates :cotent, :presence => {:message  => "Please enter post content"}
+end
